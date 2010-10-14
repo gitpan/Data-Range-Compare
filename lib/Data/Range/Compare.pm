d9c5893c721f90c28f897fdc9ead3fc2c51055de
Michael Shipper <akalinux@cpan.org>
2010-10-14 22:25:46 +0000

    Import of AKALINUX/Data-Range-Compare-1.006 from CPAN.
    gitpan-cpan-distribution: Data-Range-Compare
    gitpan-cpan-version:      1.006
    gitpan-cpan-path:         AKALINUX/Data-Range-Compare-1.006.tar.gz
    gitpan-cpan-author:       AKALINUX
    gitpan-cpan-maturity:     released

--- lib/Data/Range/Compare.pm
+++ lib/Data/Range/Compare.pm
@@ -6,7 +6,7 @@ use vars qw($VERSION @ISA @EXPORT @EXPORT_OK %EXPORT_TAGS);
 use overload '""'=>\&notation ,fallback=>1;
 
 require Exporter;
-$VERSION=1.004;
+$VERSION=1.006;
 
 @ISA=qw(Exporter);
 
---
+++ lib/Data/Range/Compare/Cookbook.pm
@@ -0,0 +1,39 @@
+=head1 NAME
+
+Data::Range::Compare::Cookbook --  Tutorials and Recipes
+
+=head1 SYNOPSIS
+
+  perldoc Data::Range::Compare::Cookbook
+
+=head1 RECIPES
+
+=over 3
+
+=item * Data::Range::Compare::Cookbook::Recipe_a_to_z
+
+This recipe is a basic tutorial on how to use the Data::Range::Compare framework to manipulate ranges, using a .. z as the range example.
+
+=item * Data::Range::Compare::Cookbook::Recipe_subclass_a_to_z
+
+This recipe builds on Data::Range::Compare::Cookbook::Recipe_a_to_z and demonstrates how to subclass Data::Range::Compare in a new module and provides a demo script explaining how to use the new module.
+
+=back
+
+=head1 AUTHOR
+
+Michael Shipper
+
+=head1 COPYRIGHT
+
+Copyright 2010 Michael Shipper.  All rights reserved.
+
+This library is free software; you can redistribute it and/or
+modify it under the same terms as Perl itself.
+
+=head1 SEE ALSO
+
+perldoc(1).
+
+=cut
+
