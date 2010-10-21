4bdb0b32d7bde57c1551241e96151f56999c8323
Michael Shipper <akalinux@cpan.org>
2010-10-21 14:52:34 +0000

    Import of AKALINUX/Data-Range-Compare-1.026 from CPAN.
    gitpan-cpan-distribution: Data-Range-Compare
    gitpan-cpan-version:      1.026
    gitpan-cpan-path:         AKALINUX/Data-Range-Compare-1.026.tar.gz
    gitpan-cpan-author:       AKALINUX
    gitpan-cpan-maturity:     released

--- lib/Data/Range/Compare.pm
+++ lib/Data/Range/Compare.pm
@@ -6,7 +6,7 @@ use vars qw($VERSION @ISA @EXPORT @EXPORT_OK %EXPORT_TAGS);
 use overload '""'=>\&notation ,fallback=>1;
 
 require Exporter;
-$VERSION=1.024;
+$VERSION=1.026;
 
 @ISA=qw(Exporter);
 
